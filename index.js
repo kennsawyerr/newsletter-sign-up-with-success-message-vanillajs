cf89081de077239a9e4f6e8062b7a82fb1aa878f
Kehinde Sawyerr <kensawyerr@gmail.com>
2024-02-16 02:49:26 +0100

    modified:   index.html 	modified:   index.js 	modified:   style.css

--- index.js
+++ index.js
@@ -2,50 +2,51 @@
 
 let inputVal = document.getElementById("inputVal");
 const subscribeBtn = document.getElementById("subBtn");
+const successMessage = document.getElementById("successMessage");
+const htmlMessageViewer = document.getElementById("message");
 
 subscribeBtn.addEventListener("click", () => {
-  console.log(inputVal.value);
-
-  if (inputVal.value === "") {
-    console.log("empty");
+  // alert(inputVal.value);
+  let emailVal = inputVal.value;
+  // alert(emailVal);
+  if (emailVal === "") {
+    errorMessage("Please provide your email address");
   } else {
-    const forbiddenArray = [",", "-", ":", ";"];
-    if (forbiddenArray.includes(inputVal.value)) {
-      return relert("please input a valid email");
+    const forbiddenArray = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (forbiddenArray.test(emailVal)) {
+      successNotificationMessage;
+    } else {
+      return errorMessage("please input a valid email");
     }
   }
 });
 
-function newChange (){
-    // this is a dummy function
-}
-
-function relert(info) {
-  alert(info);
-}
-
-// input value
-
-// an array of unwanted characters to test if its correct
-// error message
-// sucess message
-
 /* 
-user inputs email  DONE
-clicks subscribe button 
-on subscribe button click email first checks if it is correct using regex mthds
-
-{
-    gets value of input,
-    compare it with the function expressions tht you have
-
-    if{ correct, succes modal}
-    else{incorrect password shall show}
-}
-button changes to subscribed
-
+ step 1: change
 
 */
+function errorMessage(info) {
+  // alert(info);
+  htmlMessageViewer.style.display = "block";
+  htmlMessageViewer.innerHTML = `<h1>${info}</h1>`;
+  setTimeout(() => {
+    htmlMessageViewer.style.display = "none";
+  }, 3500);
 
-/*  for some reason, input no dey show
+  /* 
+  
+  show in a popup box if it is good
+  
+  
+  */
+}
+
+const successNotificationMessage = () => {
+  /*
+    hide  email signup page
+    display => block    
+    */
+};
+
+/*  for some reason, input no dey show  . Edit . just found out that alert accepts only one argument and basicaly discards the reamining
  */
