86e1ad82a774e4d7f7560a6fd2775d43becf28f9
Kehinde Sawyerr <kensawyerr@gmail.com>
2024-02-17 19:59:25 +0100

    modified:   index.html 	modified:   index.js 	modified:   style.css

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 let inputVal = document.getElementById("inputVal");
 const subscribeBtn = document.getElementById("subBtn");
+const mainBdy = document.getElementById("main-bdy");
 const successMessage = document.getElementById("successMessage");
 const htmlMessageViewer = document.getElementById("email-message");
 
@@ -15,6 +16,7 @@ subscribeBtn.addEventListener("click", () => {
     const forbiddenArray = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     if (forbiddenArray.test(emailVal)) {
       successNotificationMessage("Validation successful");
+      showSubscribeClip();
     } else {
       return errorMessage("Valid email required");
     }
@@ -25,28 +27,20 @@ subscribeBtn.addEventListener("click", () => {
  step 1: change
 
 */
+
+const showSubscribeClip = () => {
+  setInterval(() => {
+    mainBdy.style.display = "none";
+    successMessage.style.display = "block";
+  }, 1000);
+};
 function errorMessage(info) {
   htmlMessageViewer.innerHTML = `<p class="red">${info}</p>`;
   inputVal.style.border = "1px solid hsl(4, 100%, 67%)";
   inputVal.style.backgroundColor = "pink";
-  // setTimeout(() => {
-  //   inputVal.style.border = "1px solid black";
-  //   inputVal.style.backgroundColor = "inherit";
-  // }, 1500);
-
-  /* 
-  
-  show in a popup box if it is good
-  
-  
-  */
 }
 
 const successNotificationMessage = (message) => {
-  /*
-    hide  email signup page
-    display => block    
-    */
   htmlMessageViewer.innerHTML = `<p class="green">${message}</p>`;
   inputVal.style.border = "1px solid green";
   inputVal.style.backgroundColor = " rgb(182, 252, 182)";
